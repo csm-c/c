4dd4240bf7d3098108aaa1a9b9994ea71f86518b
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-11-27 21:22:46 +0530

    add about, version commands

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const VERSION = "0.0.4"
 
 func main() {
-	fmt.Println("rewrite in go!")
+	//cli args
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("c", VERSION)
+		fmt.Println("A build tool for c (like cargo for rust).")
+		fmt.Println()
+		fmt.Println("On Usage:")
+		fmt.Println(" c help")
+	} else if len(args) == 1 {
+		if args[0] == "version" || args[0] == "-v" || args[0] == "--version" {
+			fmt.Println("c", VERSION)
+		}
+	}
 }
